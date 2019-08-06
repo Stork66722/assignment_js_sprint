6ab4c499e4dcbafa0b9de91f76a447493a0f35c7
Richard L <Stork66722@gmail.com>
2019-08-06 14:31:53 -0700

    compare arrays //rlahaie

--- scripts.js
+++ scripts.js
@@ -1,36 +1,45 @@
 // FILL IN THE FUNCTIONS BELOW
 
 var sprintFunctions = {
-  largestEl: function(array){
-    return Math.max.apply( Math, array );
-  },
-  
-  reversed: function(string){  
-    return string.split("").reverse().join("");
-  },
+    largestEl: function (array) {
+        return Math.max.apply(Math, array);
+    },
 
-  loudSnakeCase: function(string){
-    string = string.replace(/[^\w\s]/gi, '').replace(/ +(?= )/g,'').split(' ');
-    string = string.map(function(el) {
-      return el.charAt(0).toUpperCase() + el.slice(1);
-    });
-    return string.join("_");
-  },
+    reversed: function (string) {
+        return string.split("").reverse().join("");
+    },
 
-  compareArrays: function(){ 
-    // your code here (replace the return)
-    return "Finish compareArrays first!" 
-  },
+    loudSnakeCase: function (string) {
+        string = string.replace(/[^\w\s]/gi, '').replace(/ +(?= )/g, '').split(' ');
+        string = string.map(function (el) {
+            return el.charAt(0).toUpperCase() + el.slice(1);
+        });
+        return string.join("_");
+    },
 
-  fizzBuzz: function(){  
-    // your code here
-  },
+    compareArrays: function (arrayOne, arrayTwo) {
+        let works = false
+        if (arrayOne.length === arrayTwo.length) {
+            for (i = 0; i < arrayOne.length; i++) {
+                if (arrayOne[i] != arrayTwo[i]) {
+                    break
+                } else if (i === (arrayOne.length - 1)) {
+                    result = true
+                }
+            }
+        }
+        return works
+    },
 
-  myMap: function(){  
-    // your code here
-  },
+    fizzBuzz: function () {
+        // your code here
+    },
 
-  primes: function(){  
-    // your code here
-  },
+    myMap: function () {
+        // your code here
+    },
+
+    primes: function () {
+        // your code here
+    },
 }
